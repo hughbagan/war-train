9728678b44b4f5de4b5375dc5c13b5da46d14141
hughbagan <25750667+hughbagan@users.noreply.github.com>
2024-03-09 18:28:51 -0700

    Make gun_car fire in a burst pattern
    Meant to be many bullets with small damage
    
    taken from mech-survivors

--- scenes/gun_car/gun_car.gd
+++ scenes/gun_car/gun_car.gd
@@ -1,8 +1,13 @@
 class_name GunCar extends TrainCar
 
-@onready var shoot_timer:Timer = $ShootTimer
 @onready var turret:Sprite2D = $Turret
 @onready var barrel:Node2D = $Turret/Barrel
+var shoot_timer:float = 0.0
+var burst_timer:float = 0.0
+var shoot_interval:float = 1.5 # time between rounds in seconds
+var burst_interval:float = 0.05 # time between individual shots
+var burst_rounds:int = 0
+var burst_max:int = 4
 
 
 func _ready():
@@ -14,6 +19,16 @@ func _physics_process(delta):
 
 	var dir_to_mouse = global_position.direction_to(get_global_mouse_position()).normalized()
 	turret.rotation = dir_to_mouse.rotated(-PI*0.5).angle()
-	if Input.is_action_pressed("shoot") and shoot_timer.is_stopped():
-		level_ref.add_child(Bullet.construct(barrel.global_position, dir_to_mouse))
-		shoot_timer.start()
+
+	shoot_timer += delta
+	burst_timer += delta
+
+	if Input.is_action_pressed("shoot"):
+		if shoot_timer >= shoot_interval and burst_rounds == 0:
+			burst_rounds = burst_max
+		elif shoot_timer >= shoot_interval and burst_rounds > 0 and burst_timer >= burst_interval:
+			level_ref.add_child(Bullet.construct(barrel.global_position, dir_to_mouse))
+			burst_timer = 0.0
+			burst_rounds -= 1
+			if burst_rounds == 0:
+				shoot_timer = 0.0
